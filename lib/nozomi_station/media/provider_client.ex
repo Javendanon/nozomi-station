138368f7ca32c9872544b5b0ff308c667e395f09
Juan Avendaño <javendano.n@gmail.com>
2026-08-15 03:09:27 -0400

    feat(media): resolve Spotify through fixed provider APIs

--- lib/nozomi_station/media/provider_client.ex
+++ lib/nozomi_station/media/provider_client.ex
@@ -1,5 +1,8 @@
 defmodule NozomiStation.Media.ProviderClient do
   @youtube_videos "https://www.googleapis.com/youtube/v3/videos"
+  @youtube_search "https://www.googleapis.com/youtube/v3/search"
+  @spotify_token "https://accounts.spotify.com/api/token"
+  @spotify_tracks "https://api.spotify.com/v1/tracks/"
 
   def fetch(provider, value, request \\ &Req.request/1)
 
@@ -28,5 +31,50 @@ defmodule NozomiStation.Media.ProviderClient do
     end
   end
 
+  def fetch(:youtube_search, query, request) do
+    options = [
+      method: :get,
+      url: @youtube_search,
+      params: [part: "snippet", type: "video", maxResults: 1, q: query, key: youtube_key()],
+      redirect: false,
+      receive_timeout: 10_000
+    ]
+
+    case request.(options) do
+      {:ok, %Req.Response{status: 200, body: %{"items" => [%{"id" => %{"videoId" => id}} | _]}}} ->
+        {:ok, id}
+
+      _ ->
+        {:error, :provider_unavailable}
+    end
+  end
+
+  def fetch(:spotify, id, request) do
+    with {:ok, %Req.Response{status: 200, body: %{"access_token" => token}}} <-
+           request.(
+             method: :post,
+             url: @spotify_token,
+             form: [grant_type: "client_credentials"],
+             auth: {:basic, "#{spotify_id()}:#{spotify_secret()}"},
+             redirect: false,
+             receive_timeout: 10_000
+           ),
+         {:ok, %Req.Response{status: 200, body: track}} <-
+           request.(
+             method: :get,
+             url: @spotify_tracks <> id,
+             auth: {:bearer, token},
+             redirect: false,
+             receive_timeout: 10_000
+           ),
+         %{"name" => title, "artists" => [%{"name" => artist} | _]} <- track do
+      {:ok, %{title: title, artist: artist}}
+    else
+      _ -> {:error, :provider_unavailable}
+    end
+  end
+
   defp youtube_key, do: Application.fetch_env!(:nozomi_station, :youtube_api_key)
+  defp spotify_id, do: Application.fetch_env!(:nozomi_station, :spotify_client_id)
+  defp spotify_secret, do: Application.fetch_env!(:nozomi_station, :spotify_client_secret)
 end
